36e7ef37d99ffe7262bab82adcd123a61d97c1d6
chaymae-jhr <chaymae.eljouhari@gmail.com>
2021-11-18 17:22:30 -0500

    generate random key algorithm added

---
+++ Exemples/Génération d’une chaine aléatoire/main.py
@@ -0,0 +1,11 @@
+import  base64
+from typing import ByteString
+from Crypto.Random import get_random_bytes
+def randgen(numbytes,algo):
+    key = get_random_bytes(numbytes) # generate a random kay of numbytes bytes
+    if algo=='base64':
+        return base64.b64encode(key).decode("utf-8")
+    if algo=='hex':
+        return key.hex()
+    else:
+        return 'error : unexistant algorithm'
