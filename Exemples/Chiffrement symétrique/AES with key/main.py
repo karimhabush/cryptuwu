a95514985860582c9463586ce237a1285466603f
chaymae-jhr <chaymae.eljouhari@gmail.com>
2021-11-21 06:54:36 -0500

    AES encryption and decryption with key file added

--- Exemples/Chiffrement symétrique/AES with key/main.py
+++ Exemples/Chiffrement symétrique/AES with key/main.py
@@ -10,45 +10,37 @@ pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
                 chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
-def output_file(output,filepath):
-    try : 
-        outputf=filepath+".enc"
-        f_output = open(outputf,"w")
-        f_output.write(output("utf-8"))
-        f_output.close()
-    except Exception as e: 
-        print(str(e))
-
-def encrypt(filepath):
+def encrypt(filepath,keypath):
     try:
-        password = input('Password..: ')
-        key = md5(password.encode('utf8')).hexdigest()
+        keyfile=open(keypath,'rb')
+        key = keyfile.read()
         f_input = open(filepath, 'rb') #open binary file in read mode
         raw = f_input.read()
         raw = pad(raw.decode('utf8'))
-        cipher = AES.new(key.encode("utf8"), AES.MODE_ECB)
+        cipher = AES.new(key, AES.MODE_ECB)
         f_output = open(filepath+".enc","w")
         f_output.write(b64encode(cipher.encrypt(raw.encode('utf8'))).decode("utf-8"))
+        keyfile.close()
         f_input.close()
         f_output.close()
     except Exception as e: 
         print(str(e))
 
-def decrypt(filepath):
+def decrypt(filepath,keypath):
     try:
-        password = input('Password..: ')
-        key = md5(password.encode('utf8')).hexdigest()
-        print(key)
+        keyfile=open(keypath,'rb')
+        key = keyfile.read()
         f_input = open(filepath, 'rb') #open binary file in read mode
         enc = f_input.read()
         enc = b64decode(enc)
-        cipher = AES.new(key.encode("utf8"), AES.MODE_ECB)
+        cipher = AES.new(key, AES.MODE_ECB)
         out = unpad(cipher.decrypt(enc)).decode('utf8')
         f_output = open(filepath+".dec","w")
         f_output.write(out)
+        keyfile.close()
         f_input.close()
         f_output.close()
     except Exception as e:
         print(str(e))
 
-decrypt('test.txt.enc')
+decrypt('test.txt.enc','keytest.key')
