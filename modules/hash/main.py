244df4a88ff5ef9354667fb04ce836e7f4378a38
karimhabush <karim.habush@gmail.com>
2021-11-25 00:20:59 +0100

    Hashing files functions added

--- modules/hash/main.py
+++ modules/hash/main.py
@@ -4,27 +4,45 @@ from Crypto.Hash import MD5
 from Crypto.Hash import SHA512
 from Crypto.Hash import SHA256
 
-def sha224(text):
+def sha224(filepath):
+    f_input = open(filepath, 'rb')
+    f_read = f_input.read()
     h = SHA224.new()
-    h.update(text.encode("utf8"))
+    h.update(f_read)
     print(h.hexdigest())
+    f_input.close()
 
-def sha512(text):
+def sha512(filepath):
+    f_input = open(filepath, 'rb')
+    f_read = f_input.read()
     h = SHA512.new()
-    h.update(text.encode("utf8"))
+    h.update(f_read)
     print(h.hexdigest())
+    f_input.close()
 
-def sha256(text):
+def sha256(filepath):
+    f_input = open(filepath, 'rb')
+    f_read = f_input.read()
     h = SHA256.new()
-    h.update(text.encode("utf8"))
+    h.update(f_read)
     print(h.hexdigest())
+    f_input.close()
 
-def md5(text):
+def md5(filepath):
+    f_input = open(filepath, 'rb')
+    f_read = f_input.read()
     h = MD5.new()
-    h.update(text.encode("utf8"))
+    h.update(f_read)
     print(h.hexdigest())
+    f_input.close()
 
-def md2(text):
+def md2(filepath):
+    f_input = open(filepath, 'rb')
+    f_read = f_input.read()
     h = MD2.new()
-    h.update(text.encode("utf8"))
+    h.update(f_read)
     print(h.hexdigest())
+    f_input.close()
+
+
+md2("./test.txt")
