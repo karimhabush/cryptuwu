1a80dded941d5542c96a654e1760985b09e3dedc
chaymae-jhr <chaymae.eljouhari@gmail.com>
2021-11-25 16:08:15 -0500

    DSA key generation, signature and sig verification added

---
+++ modules/sign/dsa/main.py
@@ -0,0 +1,56 @@
+from os import fdopen, read
+from Crypto.PublicKey import DSA
+from Crypto.Signature import DSS
+from Crypto.Hash import SHA256
+from base64 import b64decode
+from base64 import b64encode
+
+############## SIGN ##########################
+# Create a new DSA key
+def dsa_keypair(private_path, public_path):
+    private_out = open(private_path,"wb")
+    public_out = open(public_path,"wb")
+    key = DSA.generate(2048)
+    private_out.write(key.export_key())
+    public_out.write(key.publickey().export_key())
+    private_out.close()
+    public_out.close()
+
+# Sign a message
+def dsa_sign(filepath,keypath):
+    file_in = open(filepath,"rb")
+    sig_file = open(filepath+'.sig','w')
+    data = file_in.read()
+    hash_obj = SHA256.new(data)
+    key_file = open(keypath,"r")
+    pv_key = DSA.import_key(key_file.read())
+    signer = DSS.new(pv_key, 'fips-186-3')
+    sig_file.write(b64encode(signer.sign(hash_obj)).decode('utf8'))
+    sig_file.close()
+    file_in.close()
+    key_file.close()
+
+############## VERIFY ##########################
+
+
+# Verify the authenticity of the message
+def dsa_verify(message_path,keypath,sig_path):
+    try:
+        f = open(keypath, "r")
+        message_file= open(message_path,"rb")
+        message = message_file.read()
+        hash_obj = SHA256.new(message)
+        pub_key = DSA.import_key(f.read())
+        verifier = DSS.new(pub_key, 'fips-186-3')
+        sig_file = open(sig_path,'rb')
+        sig= sig_file.read()
+        sig = b64decode(sig)
+        verifier.verify(hash_obj,sig)
+        print("The message is authentic.")
+        f.close()
+        sig_file.close
+    except ValueError:
+        print("The message is not authentic.")
+
+
+  
