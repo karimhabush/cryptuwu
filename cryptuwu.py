2977fd4538f4091595cd58a1ed1f3b08876cc1ef
chaymae-jhr <chaymae.eljouhari@gmail.com>
2021-11-26 17:42:09 -0500

    Keygen and asym arguments added

--- cryptuwu.py
+++ cryptuwu.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3.9
 
 import argparse
+from modules.asym.rsa import rsa_decrypt, rsa_encrypt
 from modules.rand.random import randgen
 from modules.hash.hash import sha224, sha256, sha512, md2, md5
 from modules.encoder.encode import encode_base64, decode_base64
 from modules.sym import aes, key_aes
-
+from modules.keygen import generate
 def main():
     parser = argparse.ArgumentParser(description='add, modify and delete upstream nodes')
     parser.add_argument(
-        'mod', choices=['rand', 'sym', 'asym','hash','encode'], help='Choose a command to execute.')
+        'mod', choices=['rand', 'sym', 'asym','hash','encode','keygen'], help='Choose a command to execute.')
     parser.add_argument(
-        '-H', '--hash', choices=['sha224', 'sha256', 'sha512','md2','md5'], help='select the type of encoding')
+        '-H', '--hash', choices=['sha224', 'sha256', 'sha512','md2','md5'], help='select the type of hash')
     parser.add_argument(
         '-t', '--type', choices=['base64', 'hex'], help='select the type of encoding')
     parser.add_argument(
         '--stype', choices=['aes', 'des3'], help='select the type of encoding')
+    parser.add_argument(
+        '--ktype', choices=['rsa', 'dsa'], help='select the type of encoding')
     parser.add_argument(
         '--do', choices=['encrypt', 'decrypt'], help='select the type of encoding')
     parser.add_argument(
@@ -68,6 +71,34 @@ def main():
                     aes.decrypt(args.input)
                 else : 
                     key_aes.decrypt(args.input,args.key)
+    
+    elif args.mod == "asym":
+        if args.do is None : 
+            print("the parameter --do is required!")
+        if args.input is None:
+            print("the parameter --input is required!")
+        if args.do == "encrypt":
+            if args.key is None :
+                print("the parameter --key is required, please enter a valid public key path")
+            else:
+                rsa_encrypt(args.input, args.key)
+        elif args.do == "decrypt":
+            if args.key is None :
+                print("the parameter --key is required, please enter a valid private key path")
+            else:
+                rsa_decrypt(args.input, args.key)
+
+    elif args.mod == "keygen": 
+        if args.size is None : 
+            print("the parameter --size is required! [indicates size of the key. example : 2048]")
+        elif args.ktype is None : 
+            print("the parameter --ktype is required! [indicates type of the key. choices : rsa, dsa]")
+        elif args.ktype == "rsa":
+            generate.rsa_keypair(args.size)
+        elif args.ktype == "dsa":
+            generate.dsa_keypair(args.size)
+
+        
     
 if __name__ == "__main__":
     main()
--- modules/keygen/generate.py
+++ modules/keygen/generate.py
@@ -12,22 +12,13 @@ def rsa_keypair(size):
     f.close()
     fpb.close()
 
-
-def generate_rsa_public_key(private):
-    fpr = open(private,'r')
-    prkey = RSA.import_key(fpr.read())
-    fpb = open('./public.pem','wb')
-    pbkey = prkey.publickey().export_key('PEM')
-    fpb.write(pbkey)
-    fpb.close()
-    fpr.close()
-
 # Create a new DSA key
-def dsa_keypair(private_path, public_path):
+def dsa_keypair(size):
     private_out = open("./dsa_private.pem","wb")
     public_out = open("./dsa_public.pem","wb")
-    key = DSA.generate(2048)
+    key = DSA.generate(size)
     private_out.write(key.export_key())
     public_out.write(key.publickey().export_key())
     private_out.close()
     public_out.close()
+
